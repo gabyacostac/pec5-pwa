53eec244316a9f7ca7841aad512299c86cd3ad39
[Gabriela Carolina Canosa Acosta] <[gabrielacarolina@uoc.edu.es]>
2025-06-01 00:55:33 +0100

    artwork list component first draft

--- src/app/components/artwork-list/artwork-list.component.ts
+++ src/app/components/artwork-list/artwork-list.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Artwork } from '../../models/artwork.interface';
+import { MuseumService } from '../../services/museum.service';
 
 @Component({
   selector: 'app-artwork-list',
   standalone: false,
   templateUrl: './artwork-list.component.html',
-  styleUrl: './artwork-list.component.css'
+  styleUrl: './artwork-list.component.css',
 })
-export class ArtworkListComponent {
+export class ArtworkListComponent implements OnInit {
+  artworks: Artwork[] = [];
 
+  constructor(private museumService: MuseumService) {}
+
+  ngOnInit(): void {
+    this.museumService.getAllArtworks(20).subscribe((artworksPage) => {
+      console.log(artworksPage);
+      console.log(artworksPage.data);
+      this.artworks = artworksPage.data;
+    });
+  }
 }
