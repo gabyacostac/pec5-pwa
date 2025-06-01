cdc78ee579b38d80e5c28f3c4f4e62abcf52706c
[Gabriela Carolina Canosa Acosta] <[gabrielacarolina@uoc.edu.es]>
2025-06-01 16:59:37 +0100

    add iiif_url to the artworks object

--- src/app/components/artwork-list/artwork-list.component.ts
+++ src/app/components/artwork-list/artwork-list.component.ts
@@ -10,14 +10,25 @@ import { MuseumService } from '../../services/museum.service';
 })
 export class ArtworkListComponent implements OnInit {
   artworks: Artwork[] = [];
+  iiif_url: string = '';
+  isTable: boolean = true;
 
   constructor(private museumService: MuseumService) {}
 
   ngOnInit(): void {
     this.museumService.getAllArtworks(20).subscribe((artworksPage) => {
-      console.log(artworksPage);
-      console.log(artworksPage.data);
+      console.log('this.artworks ->', this.artworks);
+      console.log('artworksPage ->', artworksPage);
+      console.log('artworksPage.data ->', artworksPage.data);
+
       this.artworks = artworksPage.data;
+      this.iiif_url = artworksPage.config.iiif_url;
+      this.artworks.forEach((artwork) => {
+        artwork.iiif_url =
+          this.iiif_url + '/' + artwork.image_id + '/full/843,/0/default.jpg';
+      });
+
+      console.log('this.artworks ->', this.artworks);
     });
   }
 }
