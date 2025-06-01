51c5e06ee4661473bc4742b3e1dd1c1b3166a961
[Gabriela Carolina Canosa Acosta] <[gabrielacarolina@uoc.edu.es]>
2025-06-01 21:02:18 +0100

    isolate fields part of the url string

--- src/app/services/museum.service.ts
+++ src/app/services/museum.service.ts
@@ -9,25 +9,22 @@ import { ArtworksPage } from '../models/artwork-page.interface';
 })
 export class MuseumService {
   private api_url: string;
+  private fields: string;
   constructor(private http: HttpClient) {
     this.api_url = 'https://api.artic.edu/api/v1/artworks';
+    this.fields =
+      'id,title,artist_title,date_display,image_id,category_titles,description';
   }
 
   getAllArtworks(limit: number): Observable<ArtworksPage> {
     return this.http.get<ArtworksPage>(
-      this.api_url +
-        '?limit=' +
-        limit +
-        '&fields=id,title,artist_display,date_display,image_id,category_titles'
+      this.api_url + '?limit=' + limit + '&fields=' + this.fields
     );
   }
 
   getArtworkById(id: string): Observable<ArtworkData> {
     return this.http.get<ArtworkData>(
-      this.api_url +
-        '/' +
-        id +
-        '?fields=id,title,artist_display,date_display,image_id,category_titles'
+      this.api_url + '/' + id + '?fields=' + this.fields
     );
   }
 }
