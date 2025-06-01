8caa34cca6366c28010911d28f63542394e4332d
[Gabriela Carolina Canosa Acosta] <[gabrielacarolina@uoc.edu.es]>
2025-06-01 21:02:44 +0100

    fixing artwoks fields

--- src/app/models/artwork.interface.ts
+++ src/app/models/artwork.interface.ts
@@ -1,9 +1,10 @@
 export interface Artwork {
   id: string;
   title: string;
-  artist_display: string;
+  artist_title: string;
   date_display: string;
   image_id: string;
   iiif_url: string;
   category_titles: string[];
+  description: string;
 }
